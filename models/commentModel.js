e9338364e83eff6ed62f7daa3eda14a99fca9f78
Wasi566 <mohdwasiullahshaikh7@gmail.com>
2024-12-25 17:41:25 +0530

    Your commit message

--- models/commentModel.js
+++ models/commentModel.js
@@ -5,8 +5,7 @@ const commentSchema = new mongoose.Schema({
     post:{
         type: mongoose.Schema.Types.ObjectId,
         ref:"Post" , //reference to the post model 
-
-    }
+ }
     ,
     user : {
         type : String ,
